ed76ce818ec7d91e65c6bc9942b2a86659916780
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-03-24 18:37:26 +0000

    -a

--- main.cpp
+++ main.cpp
@@ -1,47 +1,36 @@
 #include <iostream>
 #include <iomanip>
 
-
 using namespace std;
 
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[])
+{
   int *pa, *pb, i, temp;
-  int SIZE = argc - 1; 
+  int SIZE = argc - 1;
   pa = new int[SIZE];
-for (i = 0; i < SIZE ; i ++)
-{
-  *pa = atoi(argv[i+1]);
-  *pa++;
-}
-*pa-=i;
-/*
-cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+  pb = new int[SIZE];
+  for (i = 0; i < SIZE; i++)
+  {
+    *pa = atoi(argv[i + 1]);
+    *pa++;
+    *pb = atoi(argv[i + 1]);
+    *pb++;
+  }
+  *pa -= i;
+  for (i = 0; i < SIZE / 2; i++)
+  {
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+    pa++;
+    pb--;
+  }
+  pa -= i;
 
-pa = &a[0];  
-pb = &a[SIZE-1];
-*/
-pb = new int[SIZE];
-pb+=i;
-for (i=0; i<SIZE/2; i++)
-{
-   temp = *pa;  
-   *pa = *pb;  
-   *pb = temp;
-   pa++;  pb--;
-}
-pa-=i;
-cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+  for (i = 0; i < SIZE - 1; i++, pa++)
+  {
+    cout << setw(3) << *pa;
+  }
 
-
-return 0;
+  return 0;
 }
